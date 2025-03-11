b84c1f39d4237effc9320fc53e7607e2de98e77d
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2025-03-11 12:14:44 +0100

    Revert action (#66)
    * Change base algorithm functions
    
    * Change perform_action_cached! to revert_action
    
    * Change perform_action_cached! to revert_action
    
    * Export revert_action

--- src/Arianna.jl
+++ src/Arianna.jl
@@ -31,7 +31,7 @@ export TXT, DAT
 
 include("metropolis.jl")
 export Action, Policy, Move
-export sample_action!, perform_action!, perform_action_cached!, invert_action!
+export sample_action!, perform_action!, revert_action!, invert_action!
 export log_proposal_density, delta_log_target_density
 export mc_step!, mc_sweep!
 export Metropolis, callback_acceptance, StoreParameters
--- src/PolicyGuided/PolicyGuided.jl
+++ src/PolicyGuided/PolicyGuided.jl
@@ -6,7 +6,7 @@ Module for policy-guided Monte Carlo algorithms.
 module PolicyGuided
 
 using ..Arianna: AriannaSystem, Action, Policy, AriannaAlgorithm, Simulation, Metropolis
-import ..Arianna: make_step!, write_algorithm, sample_action!, perform_action!, delta_log_target_density, log_proposal_density, invert_action!, perform_action_cached!, raise_error
+import ..Arianna: make_step!, write_algorithm, sample_action!, perform_action!, delta_log_target_density, log_proposal_density, invert_action!, revert_action!, raise_error
 using Random
 using LinearAlgebra
 using Transducers
