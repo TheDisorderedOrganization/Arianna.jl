161029db93e267cf30442e7d7f40298534f853da
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2025-06-13 14:58:05 +0200

    Renaming + Cleaning

--- examples/ising_1d/MC_launch.jl
+++ examples/ising_1d/MC_launch.jl
@@ -7,22 +7,22 @@ include("ising_1d.jl")
 ## RUN THE SIMULATION
 seed = 42
 rng = Xoshiro(seed)
-β = 1.0
+β = 0.5
 M = 1
 j= 0.1
 h = 0.1
-N=10
+N=100
 chains = [Ising1D(N, j, h, β) for _ in 1:M]
 pool = (Move(Flip(1), StandardUniform(), Vector{Float64}(), 1.0), )
-steps = 10^6
-burn = 10^5
-block = [0, steps ÷ 100]
+steps = 2*10^7
+burn = 10^7
+block = [0, 10^3]
 sampletimes = build_schedule(steps, burn, block)
-path = "data/MC/ising_1d/beta$(β)J$(j)H$(h)/"
+path = "examples/data/MC/ising_1d/beta$(β)J$(j)H$(h)/"
 
 algorithm_list = (
     (algorithm=Metropolis, pool=pool, seed=seed, parallel=false),
-    (algorithm=StoreCallbacks, callbacks=(callback_energy, callback_acceptance), scheduler=sampletimes),
+    (algorithm=StoreCallbacks, callbacks=(callback_energy, callback_acceptance), store_first=false, scheduler=sampletimes),
     (algorithm=StoreTrajectories, scheduler=sampletimes),
     (algorithm=PrintTimeSteps, scheduler=build_schedule(steps, burn, steps ÷ 10)),
 ) 
@@ -37,7 +37,7 @@ default(tickfontsize=15, guidefontsize=15, titlefontsize=15, legendfontsize=15,
 energies = readdlm(joinpath(path, "energy.dat"))[:, 2]
 @show mean(energies), std(energies)
 
-stephist!(energies, normalize=:pdf, lw=3, label="Simulation", c=1)
+stephist!(energies, normalize=:pdf, lw=3, c=1)
 savefig("examples/ising_1d/density.png")
 
 
